e7d8162dead649ec45ec769349ee8b58a15b79cb
Duncan Bradley <47359202+duncanbradley@users.noreply.github.com>
2026-05-06 22:04:36 +0100

    set selected by default, tweaks to wording

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,7 +32,7 @@ function calculateSlices(input, data) {
   ].filter(({ value }) => value);
 }
 
- let selected = $state()
+ let selected = $state('8pm or earlier')
 
  let newSlices = $derived(calculateSlices(selected, data.sleep))
 
@@ -48,24 +48,26 @@ function calculateSlices(input, data) {
   });
 
   const template = descriptions[closestThreshold];
-  return template.replace('{value}', value);
+  return template.replace('{value}', Math.round(value));
 }
 
  let descriptions = $state({
-  9: 'only {value}% of people',
-  10: 'about one in ten people',
-  15: 'about one in seven people',
-  20: 'about a fifth of people',
-  25: 'about a quarter of people',
-  33: 'about a third of people',
-  40: 'just less than half of people',
-  50: 'about half of people',
-  60: 'just more than half of people',
-  67: 'about two thirds of people',
-  75: 'about three quarters of people',
-  80: 'about four fifths of people',
-  85: 'about six in seven people',
-  90: 'about nine in ten people',
+  9: 'Only {value}% of people',
+  10: 'About one in ten people',
+  15: 'About one in seven people',
+  20: 'About a fifth of people',
+  25: 'About a quarter of people',
+  33: 'About a third of people',
+  40: 'Just over a third of people',
+  45: 'Just less than half of people',
+  50: 'About half of people',
+  55: 'Just over half of people',
+  60: 'Just under two thirds of people',
+  67: 'About two thirds of people',
+  75: 'About three quarters of people',
+  80: 'About four fifths of people',
+  85: 'About six in seven people',
+  90: 'About nine in ten people',
   91: '{value}% of people',
  })
 
@@ -73,14 +75,19 @@ function calculateSlices(input, data) {
     let beforeText, chosenText, afterText;
 
   if (newSlices[0].name !== 'before') {
+    console.log('first')
      chosenText = `${findClosest(newSlices[0].value, descriptions)} go to bed at the same time as you`
      afterText = `${findClosest(newSlices[1].value, descriptions)} go to bed later than you`
   }
-  else if (newSlices[1].name !== 'after') {
+  else if (newSlices[1].name === 'after') {
+        console.log('second')
+
      beforeText = `${findClosest(newSlices[0].value, descriptions)} go to bed earlier than you`
      chosenText = `${findClosest(newSlices[1].value, descriptions)} go to bed at the same time as you`
   }
   else {
+        console.log('third')
+
      beforeText = `${findClosest(newSlices[0].value, descriptions)} go to bed earlier than you`
      chosenText = `${findClosest(newSlices[1].value, descriptions)} go to bed at the same time as you`
      afterText = `${findClosest(newSlices[2].value, descriptions)} go to bed later than you`
@@ -90,8 +97,8 @@ function calculateSlices(input, data) {
 
  }
 
-// let ok = $derived(generateText(newSlices))
-let textArray = ["r", "o", "l"]
+let textArray = $derived(generateText(newSlices))
+// let textArray = ["r", "o", "l"]
 
 </script>
 <h1>What time do you go to bed?</h1>
