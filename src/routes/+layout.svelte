8dda9ce9f55e29ff6ff8cdbc492e283e84aac7ac
Duncan Bradley <47359202+duncanbradley@users.noreply.github.com>
2026-06-06 09:24:14 +0100

    add h1 to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,28 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+<div class="page">
+<div class="content">
+<h1 id="question">
+  What time do you go to bed?
+    <span>On average</span>
+</h1>
 {@render children()}
+
+</div>
+</div>
+
+
+<style>
+  h1 {
+color: aliceblue;
+font-family: 'Apfel Grotezk Fett'
+}
+
+.page{
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
