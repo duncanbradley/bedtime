57f1b4d8d119d9b483fc9cf5369f4e3072a79e51
Duncan Bradley <duncan.bradley@communities.gov.uk>
2026-06-12 21:56:36 +0100

    push build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const dev = process.argv.includes('dev');
 
@@ -16,7 +15,6 @@ const config = {
 			base: dev ? '' : process.env.BASE_PATH,
 		}
     },
-    preprocess: vitePreprocess()
 };
 
 export default config;
