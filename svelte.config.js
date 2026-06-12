bad5cbc6ac12bd54d9682445efb4fd8e5b7b4ecb
Duncan Bradley <duncan.bradley@communities.gov.uk>
2026-06-12 21:45:50 +0100

    add gh params

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,11 +10,13 @@ const config = {
 		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
 	},
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+        appDir: 'app', // Required as the default is _app
+        adapter: adapter(),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
+    },
+    preprocess: vitePreprocess()
 };
 
 export default config;
